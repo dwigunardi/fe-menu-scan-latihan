72f0b21500a352162ec7f6dec3bd2d99d5da7ffa
dwigunardi <dwigunardi@gmail.com>
2026-08-25 23:48:14 +0700

    feat(api): add unified barrel export for src/lib/api

---
+++ src/lib/api/index.ts
@@ -0,0 +1,21 @@
+// Core Transport & Fetchers
+export * from './custom-fetch';
+export * from './hardened-fetch';
+export * from './either';
+export * from './pipeline/pipeline-runner';
+export * from './pipeline/types';
+
+// Error Handling & Notification
+export * from './api-error';
+export * from './error-codes';
+export * from './error-translator';
+export * from './notify-error';
+
+// Domain Client APIs
+export * from './auth-api';
+export * from './admin-menus-api';
+export * from './admin-orders-api';
+export * from './admin-tables-api';
+export * from './admin-banners-api';
+export * from './admin-reports-api';
+export * from './media-api';
